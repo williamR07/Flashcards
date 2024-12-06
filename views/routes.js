9858a6393aa4b590d066ac002fa0ed936012a71f
Tarik Delalic <48967705+dela777@users.noreply.github.com>
2024-12-06 15:50:51 +0100

    Update

--- views/routes.js
+++ views/routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { getRequest } = require('../controlers/controlers')
+const { getRequest } = require('../controllers/controllers')
 
 router.get('/question', getRequest);
 
